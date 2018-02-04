693e071bb8dde9baf7d27d7d3abcc035a03baa44
Unknown <dillonbrosche@gmail.com>
2018-02-04 18:51:56 -0500

    Add GSM support

--- invictrix.mk
+++ invictrix.mk
@@ -7,7 +7,7 @@ TARGET_SCREEN_WIDTH := 1440
 # Enhanced NFC
 # $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/invictrix/config/gsm.mk
+$(call inherit-product, vendor/invictrix/config/gsm.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/invictrix/config/common.mk)
 
